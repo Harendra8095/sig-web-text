1b1bc9ba914e4476adabf9170ef93329a86c6bb8
Harendra8095 <harendrarathor444@gmail.com>
2020-09-15 23:01:23 +0530

    Database initialized now working on migration

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask
 import os
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+from models import Result
 
 @app.route('/')
 def hello():
--- config.py
+++ config.py
@@ -6,7 +6,8 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = 'i-put-this-later'
+    SECRET_KEY = 'this-really-needs-to-be-changed'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 class ProductionConfig(Config):
     DEBUG = False
